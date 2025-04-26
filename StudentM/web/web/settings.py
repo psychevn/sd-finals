2be6097bb83e1b5a3f9b6d6f0c0c6073ef44498f
Meekhen <esuumee08@gmail.com>
2025-04-26 18:34:57 +0800

    Add files via upload

--- StudentM/web/web/settings.py
+++ StudentM/web/web/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
